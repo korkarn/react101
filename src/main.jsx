76309f1e1101c12fbb2dbaa4eaa399841cdb3be8
Karn Worakarn <korkarn.worakrn@gmail.com>
2022-04-28 23:43:06 +0700

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Workshop from './workshop/Workshop'
 import Assignment from './assignment/todo'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Workshop/> */}
     <Assignment/>
   </React.StrictMode>
 )
